e5e33a7dd070f77ea6fcdb1bb8fe825a0192a2ed
mate00 <mami.ext@gmail.com>
2020-04-19 07:13:03 +0200

    Part 7. Error handling.

--- src/main/java/io/pivotal/literx/Part07Errors.java
+++ src/main/java/io/pivotal/literx/Part07Errors.java
@@ -33,14 +33,14 @@ public class Part07Errors {
 
 	// TODO Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.
 	Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
-		return null;
+		return mono.onErrorResume(IllegalStateException.class, e -> Mono.just(User.SAUL));
 	}
 
 //========================================================================================
 
 	// TODO Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs in the input Flux, else do not change the input Flux.
 	Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
-		return null;
+		return flux.onErrorResume(e -> Flux.just(User.SAUL, User.JESSE));
 	}
 
 //========================================================================================
