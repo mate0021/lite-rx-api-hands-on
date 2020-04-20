295944e320305431d81579d22ec818d17a1953d3
mate00 <mami.ext@gmail.com>
2020-04-20 07:10:47 +0200

    Part 7. Error handling. Wrap exception using Exceptions.propagate(e).

--- src/main/java/io/pivotal/literx/Part07Errors.java
+++ src/main/java/io/pivotal/literx/Part07Errors.java
@@ -48,7 +48,13 @@ public class Part07Errors {
 	// TODO Implement a method that capitalizes each user of the incoming flux using the
 	// #capitalizeUser method and emits an error containing a GetOutOfHereException error
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(u -> {
+			try {
+				return capitalizeUser(u);
+			} catch (GetOutOfHereException e) {
+				throw Exceptions.propagate(e);
+			}
+		});
 	}
 
 	User capitalizeUser(User user) throws GetOutOfHereException {
